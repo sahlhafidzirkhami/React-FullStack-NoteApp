0a7ceb3db8e67f435ebedad87a9b57cfa28f84f3
Sahl Hafidz <akunbelajarhafidz@gmail.com>
2024-12-18 11:47:19 +0700

    feat(layout):add csr

--- fe-note-app/pages/index.tsx
+++ fe-note-app/pages/index.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-const Component = dynamic(() => import(/* webpackChunkName: "dashboard-page" */ '../src/components/templates/Dashboard/index'), {
+const Component = dynamic(() => import(/* webpackChunkName: "dashboard-page" */ '@/components/templates/Dashboard'), {
   ssr: false,
 });
 export default Component;
